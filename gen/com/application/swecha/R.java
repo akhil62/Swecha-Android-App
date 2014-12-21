74b114ad39f4fdf05b1ed92d761eab4f19f77899
akhil <akhilreddy_62@yahoo.com>
2014-12-21 15:56:26 +0530

    List view editing and icons updated

--- gen/com/application/swecha/R.java
+++ gen/com/application/swecha/R.java
@@ -11,7 +11,6 @@ public final class R {
     public static final class array {
         /**  Nav Drawer List Item Icons 
  Keep them in order as the titles are in 
- <item>@drawable/ic_aboutus</item>
          */
         public static final int nav_drawer_icons=0x7f070001;
         /**  Nav Drawer Menu Items 
@@ -46,18 +45,19 @@ public final class R {
     public static final class drawable {
         public static final int counter_bg=0x7f020000;
         public static final int gradient_background=0x7f020001;
-        public static final int ic_communities=0x7f020002;
-        public static final int ic_drawer=0x7f020003;
-        public static final int ic_home=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_pages=0x7f020006;
-        public static final int ic_people=0x7f020007;
-        public static final int ic_photos=0x7f020008;
-        public static final int ic_whats_hot=0x7f020009;
-        public static final int list_item_bg_normal=0x7f02000a;
-        public static final int list_item_bg_pressed=0x7f02000b;
-        public static final int list_selector=0x7f02000c;
-        public static final int swecha_logo=0x7f02000d;
+        public static final int ic_about=0x7f020002;
+        public static final int ic_communities=0x7f020003;
+        public static final int ic_drawer=0x7f020004;
+        public static final int ic_home=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_pages=0x7f020007;
+        public static final int ic_people=0x7f020008;
+        public static final int ic_photos=0x7f020009;
+        public static final int ic_whats_hot=0x7f02000a;
+        public static final int list_item_bg_normal=0x7f02000b;
+        public static final int list_item_bg_pressed=0x7f02000c;
+        public static final int list_selector=0x7f02000d;
+        public static final int swecha_logo=0x7f02000e;
     }
     public static final class id {
         public static final int action_settings=0x7f0a000b;
